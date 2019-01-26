97e276a15edbcb5802c7cc1227dff15e565f2119
DESKTOP-RNCNPDF\Mati <doktormateo@gmail.com>
2019-01-26 19:15:09 +0100

    add method new words to dictionary

--- src/main/java/languageTrainer/App.java
+++ src/main/java/languageTrainer/App.java
@@ -1,15 +1,14 @@
 package languageTrainer;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.Scanner;
 
 public class App {
-    public static void main(String[] args) {
-        
-        String file = "dictionary.txt";
+    private static Scanner scanner = new Scanner(System.in);
+    private static String file = "dictionary.txt";
+
+    public static void readFromFile() {
+
         String line;
         try (FileReader fr = new FileReader(file); BufferedReader br = new BufferedReader(fr)) {
             while ((line = br.readLine()) != null) {
@@ -20,14 +19,16 @@ public class App {
 
                 System.out.println(question);
 
-                Scanner scanner = new Scanner(System.in);
 
                 String answer = scanner.next().toLowerCase();
 
                 while (!answer.equals(solution)) {
+                    scanner.close();
+
                     System.out.println("bad answer, try again");
                     answer = scanner.next();
                 }
+                scanner.close();
 
                 System.out.println("good answer");
 
@@ -37,5 +38,55 @@ public class App {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public static void saveSingleLineToFile() {
+        String newLine;
+        try (FileWriter fw = new FileWriter(file, true); BufferedWriter bw = new BufferedWriter(fw)) {
+            bw.write("\n");
+            bw.write(scanner.nextLine());
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void saveToFile() {
+        boolean flag = true;
+
+
+        while (flag) {
+            System.out.println("Choose form options:\n" +
+                    "0 -> quit\n" +
+                    "1 -> add new ");
+
+            String menu = scanner.nextLine();
+            switch (menu) {
+                case "0":
+                    System.out.println("exit");
+                    flag = false;
+                    break;
+                case "1":
+                    System.out.println("Add word with schema: \n word translation");
+                    saveSingleLineToFile();
+                    break;
+                default:
+                    System.out.println("bad input");
+                    break;
+            }
+
+        }
+    }
+
+    /*boolean flag = true;
+            c
+    int menu = scanner.nextInt();*/
+
+    public static void main(String[] args) {
+        //readFromFile();
+        saveToFile();
+
     }
 }
